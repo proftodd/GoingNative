7f45744d7e9c4ba00aa2d7dd8e831664ffae3c1d
John Todd <john.todd@leadingedje.com>
2025-09-18 16:42:27 -0400

    make a native call

--- CsRMatrix/CsRMatrix/Engine/CsRMatrixNativeAdapter.cs
+++ CsRMatrix/CsRMatrix/Engine/CsRMatrixNativeAdapter.cs
@@ -1,17 +1,29 @@
 ﻿using CsRMatrix.Model;
+using System.Runtime.InteropServices;
 
 namespace CsRMatrix.Engine;
 
-public static class CsRMatrixNativeAdapter
+public static partial class CsRMatrixNativeAdapter
 {
+    [LibraryImport("rashunal", EntryPoint = "n_Rashunal")]
+    private static partial IntPtr n_Rashunal(int numerator, int denominator);
+
     public static CsGaussFactorization Factor(int[][][] data)
     {
+        Rashunal r = Marshal.PtrToStructure<Rashunal>(n_Rashunal(1, 2));
+        CsRashunal cr = new() { Numerator = r.numerator, Denominator = r.denominator };
         return new CsGaussFactorization
         {
-            PInverse = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
+            PInverse = new Model.CsRMatrix { Height = 1, Width = 1, Data = [cr], },
             Lower = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
             Diagonal = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
             Upper = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
         };
+    }
+
+    private struct Rashunal
+    {
+        public int numerator;
+        public int denominator;
     }
 }
