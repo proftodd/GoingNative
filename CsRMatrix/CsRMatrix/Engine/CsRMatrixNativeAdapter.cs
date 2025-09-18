ebfd1bc6e859893a92b165dc0ab0db538af9db63
John Todd <john.todd@leadingedje.com>
2025-09-18 13:19:20 -0400

    get the c# code in place

---
+++ CsRMatrix/CsRMatrix/Engine/CsRMatrixNativeAdapter.cs
@@ -0,0 +1,17 @@
+﻿using CsRMatrix.Model;
+
+namespace CsRMatrix.Engine;
+
+public static class CsRMatrixNativeAdapter
+{
+    public static CsGaussFactorization Factor(int[][][] data)
+    {
+        return new CsGaussFactorization
+        {
+            PInverse = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
+            Lower = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
+            Diagonal = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
+            Upper = new Model.CsRMatrix { Height = 0, Width = 0, Data = [], },
+        };
+    }
+}
---
+++ CsRMatrix/CsRMatrix/Model/CsRMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace CsRMatrix.Model;
+
+public record CsRMatrix
+{
+    public int Height;
+    public int Width;
+    public CsRashunal[] Data = [];
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < Height; ++i)
+        {
+            sb.Append("[ ");
+            for (int j = 0; j < Width; ++j)
+            {
+                var el = Data[i * Width + j];
+                sb.Append(el.ToString());
+                sb.Append(' ');
+            }
+            sb.Append("]\n");
+        }
+        return sb.ToString();
+    }
+}
