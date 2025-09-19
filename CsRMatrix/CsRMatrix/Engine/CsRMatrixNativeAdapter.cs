47d101b7a6968293c326543784a0684f61602733
John Todd <john.todd@leadingedje.com>
2025-09-19 15:23:31 -0400

    Do it with SafeHandles to more naturally manage memory

--- CsRMatrix/CsRMatrix/Engine/CsRMatrixNativeAdapter.cs
+++ CsRMatrix/CsRMatrix/Engine/CsRMatrixNativeAdapter.cs
@@ -21,33 +21,33 @@ public static partial class CsRMatrixNativeAdapter
         public IntPtr Upper;
     }
 
-    [LibraryImport("rashunal", EntryPoint = "n_Rashunal")]
-    private static partial IntPtr n_Rashunal(int numerator, int denominator);
+    [DllImport("rashunal", EntryPoint = "n_Rashunal")]
+    private static extern RashunalHandle n_Rashunal(int numerator, int denominator);
 
-    [LibraryImport("rmatrix", EntryPoint = "new_RMatrix")]
-    private static partial IntPtr new_RMatrix(int height, int width, IntPtr data);
+    [DllImport("rmatrix", EntryPoint = "new_RMatrix")]
+    private static extern RMatrixHandle new_RMatrix(int height, int width, IntPtr data);
 
-    [LibraryImport("rmatrix", EntryPoint = "RMatrix_height")]
-    private static partial int RMatrix_height(IntPtr m);
+    [DllImport("rmatrix", EntryPoint = "RMatrix_height")]
+    private static extern int RMatrix_height(RMatrixHandle m);
 
-    [LibraryImport("rmatrix", EntryPoint = "RMatrix_width")]
-    private static partial int RMatrix_width(IntPtr m);
+    [DllImport("rmatrix", EntryPoint = "RMatrix_width")]
+    private static extern int RMatrix_width(RMatrixHandle m);
 
-    [LibraryImport("rmatrix", EntryPoint = "RMatrix_get")]
-    private static partial IntPtr RMatrix_get(IntPtr m, int row, int col);
+    [DllImport("rmatrix", EntryPoint = "RMatrix_get")]
+    private static extern RashunalHandle RMatrix_get(RMatrixHandle m, int row, int col);
 
-    [LibraryImport("rmatrix", EntryPoint = "RMatrix_gelim")]
-    private static partial IntPtr RMatrix_gelim(IntPtr m);
+    [DllImport("rmatrix", EntryPoint = "RMatrix_gelim")]
+    private static extern GaussFactorizationHandle RMatrix_gelim(RMatrixHandle m);
 
-    private static IntPtr AllocRashunal(int num, int den)
+    private static RashunalHandle AllocRashunal(int num, int den)
     {
         IntPtr ptr = NativeStdLib.Malloc((UIntPtr)Marshal.SizeOf<Rashunal>());
         var value = new Rashunal { numerator = num, denominator = den };
         Marshal.StructureToPtr(value, ptr, false);
-        return ptr;
+        return new RashunalHandle(ptr, ownsHandle: true);
     }
 
-    private static IntPtr AllocateNativeRMatrix(Model.CsRMatrix m)
+    private static RMatrixHandle AllocateNativeRMatrix(Model.CsRMatrix m)
     {
         int elementCount = m.Height * m.Width;
         IntPtr elementArray = NativeStdLib.Malloc((UIntPtr)(IntPtr.Size * elementCount));
@@ -58,21 +58,17 @@ public static partial class CsRMatrixNativeAdapter
             {
                 var element = m.Data[i];
                 var elementPtr = AllocRashunal(element.Numerator, element.Denominator);
-                pArray[i] = elementPtr;
+                pArray[i] = elementPtr.DangerousGetHandle();
             }
             var rMatrixPtr = new_RMatrix(m.Height, m.Width, elementArray);
 
-            for (int i = 0; i < elementCount; ++i)
-            {
-                NativeStdLib.Free(pArray[i]);
-            }
             NativeStdLib.Free(elementArray);
 
             return rMatrixPtr;
         }
     }
 
-    private static Model.CsRMatrix AllocateManagedRMatrix(IntPtr m)
+    private static Model.CsRMatrix AllocateManagedRMatrix(RMatrixHandle m)
     {
         int height = RMatrix_height(m);
         int width = RMatrix_width(m);
@@ -81,10 +77,9 @@ public static partial class CsRMatrixNativeAdapter
         {
             for (int j = 1; j <= width; ++j)
             {
-                var rPtr = RMatrix_get(m, i, j);
-                var r = Marshal.PtrToStructure<Rashunal>(rPtr);
+                using var rPtr = RMatrix_get(m, i, j);
+                var r = Marshal.PtrToStructure<Rashunal>(rPtr.DangerousGetHandle());
                 data[(i - 1) * width + (j - 1)] = new CsRashunal { Numerator = r.numerator, Denominator = r.denominator };
-                NativeStdLib.Free(rPtr);
             }
         }
         return new Model.CsRMatrix { Height = height, Width = width, Data = data, };
@@ -92,18 +87,19 @@ public static partial class CsRMatrixNativeAdapter
 
     public static CsGaussFactorization Factor(Model.CsRMatrix m)
     {
-        var nativeMPtr = AllocateNativeRMatrix(m);
-        var fPtr = RMatrix_gelim(nativeMPtr);
-        var f = Marshal.PtrToStructure<GaussFactorization>(fPtr);
-        var csF = new CsGaussFactorization
+        using var nativeM = AllocateNativeRMatrix(m);
+        using var fPtr = RMatrix_gelim(nativeM);
+        var f = Marshal.PtrToStructure<GaussFactorization>(fPtr.DangerousGetHandle());
+        using var pInv = new RMatrixHandle(f.PInverse, ownsHandle: true);
+        using var pLower = new RMatrixHandle(f.Lower, ownsHandle: true);
+        using var pDiagonal = new RMatrixHandle(f.Diagonal, ownsHandle: true);
+        using var pUpper = new RMatrixHandle(f.Upper, ownsHandle: true);
+        return new CsGaussFactorization
         {
-            PInverse = AllocateManagedRMatrix(f.PInverse),
-            Lower = AllocateManagedRMatrix(f.Lower),
-            Diagonal = AllocateManagedRMatrix(f.Diagonal),
-            Upper = AllocateManagedRMatrix(f.Upper),
+            PInverse = AllocateManagedRMatrix(pInv),
+            Lower = AllocateManagedRMatrix(pLower),
+            Diagonal = AllocateManagedRMatrix(pDiagonal),
+            Upper = AllocateManagedRMatrix(pUpper),
         };
-        NativeStdLib.Free(nativeMPtr);
-        NativeStdLib.Free(fPtr);
-        return csF;
     }
 }
---
+++ CsRMatrix/CsRMatrix/Engine/Handle.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CsRMatrix.Engine;
+
+internal sealed class RashunalHandle : SafeHandle
+{
+    private RashunalHandle() : base(IntPtr.Zero, ownsHandle: true) { }
+
+    internal RashunalHandle(IntPtr existing, bool ownsHandle)
+        : base(IntPtr.Zero, ownsHandle)
+        => SetHandle(existing);
+
+    public override bool IsInvalid => handle == IntPtr.Zero;
+
+    protected override bool ReleaseHandle()
+    {
+        NativeStdLib.Free(handle);
+        return true;
+    }
+}
+
+internal sealed class RMatrixHandle : SafeHandle
+{
+    private RMatrixHandle() : base(IntPtr.Zero, ownsHandle: true) { }
+
+    internal RMatrixHandle(IntPtr existing, bool ownsHandle)
+        : base(IntPtr.Zero, ownsHandle)
+        => SetHandle(existing);
+
+    public override bool IsInvalid => handle == IntPtr.Zero;
+
+    protected override bool ReleaseHandle()
+    {
+        NativeStdLib.Free(handle);
+        return true;
+    }
+}
+
+internal sealed class GaussFactorizationHandle : SafeHandle
+{
+    private GaussFactorizationHandle() : base(IntPtr.Zero, ownsHandle: true) { }
+
+    internal GaussFactorizationHandle(IntPtr existing, bool ownsHandle)
+        : base(IntPtr.Zero, ownsHandle)
+        => SetHandle(existing);
+
+    public override bool IsInvalid => handle == IntPtr.Zero;
+
+    protected override bool ReleaseHandle()
+    {
+        NativeStdLib.Free(handle);
+        return true;
+    }
+}
